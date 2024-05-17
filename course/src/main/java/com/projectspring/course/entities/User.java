9ec44a6c4726f5d5c189100bf276d0cc37906785
fabianeheckmann <fabiane.heckmanns@gmail.com>
2024-05-17 17:17:43 -0300

    :sparkles:feat: order, instant, ISO8601

--- course/src/main/java/com/projectspring/course/entities/User.java
+++ course/src/main/java/com/projectspring/course/entities/User.java
@@ -1,8 +1,11 @@
 package com.projectspring.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -16,6 +19,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    @JsonIgnore
+    private List<Order> orders = new ArrayList<>();
 
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
@@ -67,6 +74,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
--- course/src/main/java/com/projectspring/course/repositories/UserRepository.java
+++ course/src/main/java/com/projectspring/course/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.projectspring.course.repositories;
 import com.projectspring.course.entities.User;
 import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
